b9edc1d1120c581cc09e43e404ae37d43f6bc414
Aayushman Roy <aayushmankrroy098@gmail.com>
2026-01-18 16:34:12 +0545

    Removed old programs

--- Small_programs.py/sorting.py
+++
@@ -1,9 +0,0 @@
-n=int(input("How many students? = "))
-names = []
-for i in range (1,n+1):
-    name = input(f"name {i} = ")
-    names.append(name)
-print(f"Original list is = {names}")
-names.sort()
-for i in range(0,len(names)):
-    print(f"{i+1}. {names[i]}")
